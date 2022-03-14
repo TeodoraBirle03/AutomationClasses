a895e03b7dd75e357988a50ba81eadbc4dbc0afc
dan birle <teodorabirle.ppb@gmail.com>
2022-03-14 11:33:01 +0200

    changes for Curs7

--- Cursuri/Curs2/de_Retinut.py
+++ Cursuri/Curs2/de_Retinut.py
@@ -5,3 +5,5 @@ print(ultimul_index) # aici iti afiseaza un nr, nr-ul pozitiei ultimului caracte
 print(litera[len(litera)-1])# aici iti afiseaza carcterul de pe ultima pozitie
 print(litera[ultimul_index])
 print(litera[0:])# aici iti afiseaza tot string-ul
+
+

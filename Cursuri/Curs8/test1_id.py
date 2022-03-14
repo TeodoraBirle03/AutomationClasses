8c103e2b4dc2b3f3a059da23ef6798f007d09635
dan birle <teodorabirle.ppb@gmail.com>
2022-03-14 20:36:14 +0200

    Adding Curs8

---
+++ Cursuri/Curs8/test1_id.py
@@ -0,0 +1,25 @@
+# avem nevoie sa importam cateva librarii gratuite care ne ajuta sa controlam chrome
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# initializam chrome
+s = Service(ChromeDriverManager().install())
+chrome = webdriver.Chrome(service=s)
+
+# maximizam fereastra
+chrome.maximize_window()
+
+# navigam catre un url
+chrome.get('https://formy-project.herokuapp.com/form')
+
+# selector by ID
+first_name = chrome.find_element(By.ID, 'first-name')
+first_name.send_keys('Teodora')
+
+last_name = chrome.find_element(By.ID, 'last-name')
+last_name.send_keys('Birle')
+sleep(3)
+chrome.quit()
